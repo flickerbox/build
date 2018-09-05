0c1852ec17b6a55ca13a00162beda33380a67dfd
Chris Carlevato <chris@flickerbox.com>
2018-09-05 14:33:46 -0700

    Add babel plugins for more forgiving builds

--- babel.config.js
+++ babel.config.js
@@ -4,12 +4,20 @@ module.exports = {
 		"react",
 		["env", {
 			"targets": {
-				"browsers": ["> 0.5%", "last 2 versions", "iOS >= 8", "IE 10", "IE 11"]
-			}
+				"browsers": ["> 0.5%", "last 2 versions", "iOS >= 8", "IE >= 10"]
+			},
+			"useBuiltIns": "entry",
+			"debug": false
 		}]
 	],
 	"plugins": [[
+		"transform-object-assign",
+		"transform-es2015-constants",
+		"transform-es2015-classes",
+		"transform-class",
+		"transform-es2015-modules-commonjs",
 		"transform-class-properties",
 		{ "spec": true }
 	]]
 }
+
