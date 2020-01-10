06f10aaccb5f131b3ada45bd1e4f87dd7441a4dd
Chris Carlevato <chris@flickerbox.com>
2020-01-10 11:41:30 -0800

    Add initial updates for vnext

--- webpack.config.js
+++ webpack.config.js
@@ -1,117 +1,133 @@
-const path = require('path');
-const webpack = require('webpack');
-const WebpackNotifierPlugin = require('webpack-notifier');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const babelConfig = require('./babel.config');
+const babelConfig = require('./babel.config')
+const dashboard = require('webpack-dashboard/plugin')
+const globImporter = require('node-sass-glob-importer')
+const ignoreAssetsPlugin = require('ignore-assets-webpack-plugin')
+const notifier = require('webpack-notifier')
+const path = require('path')
+const vueLoader = require('vue-loader/lib/plugin')
+const webpack = require('webpak')
 
-module.exports = ( env = 'development' ) => {
+const environment = process.env.NODE_ENV
 
-	return {
+module.exports = {
 
-		mode: env,
+  mode: environment,
 
-		watch: ( env === 'development' ),
+  entry: [],
 
-		entry: [],
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'js/[name].js',
+  },
 
-		output: {
-			path: path.resolve(__dirname),
-			filename: 'js/[name].js',
-		},
+  plugins: [
+    new dashboard(),
+    new vueLoader(),
+    new ignoreAssetsPlugin({
+      ignore: ['js/css.js', 'js/css.js.map']
+    }),
+    new notifier({
+      title: 'Flickerbox Build',
+      contentImage: path.join(__dirname, notifier_logo),
+      alwaysNotify: true,
+      skipFirstNotification: false,
+      excludeWarnings: false,
+    }),
+    new webpack.DefinePlugin({
+      PRODUCTION: JSON.stringify('production' === environment),
+    }),
+  ],
 
-		plugins: [
-			new VueLoaderPlugin(),
-			new WebpackNotifierPlugin({
-				title: 'Flickerbox Build Runner',
-				contentImage: path.join(__dirname, 'notifier.png'),
-				alwaysNotify: true,
-				skipFirstNotification: false,
-				excludeWarnings: false
-			}),
-			new webpack.DefinePlugin({
-				PRODUCTION: JSON.stringify(env === 'production'),
-			}),
-		],
+  devtool: ( 'production' === environment ? '' : 'source-map' ),
 
-		devtool: 'source-map',
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      'fabric': require('@flickerbox/fabric/webpack.manifest').sassDir,
+      'pacnav': require('@flickerbox/pacnav/webpack.manifest').sassDir,
+    },
+    extensions: ['*', '.js', '.vue', '.json'],
+  },
 
-		resolve: {
-			alias: {
-				'vue$': 'vue/dist/vue.esm.js',
-				'fabric': require('@flickerbox/fabric/webpack.manifest').sassDir,
-				'pacnav': require('@flickerbox/pacnav/webpack.manifest').sassDir,
-			},
-			extensions: ['*', '.js', '.vue', '.json'],
-		},
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'vue-style-loader' },
+          { loader: 'css-loader' }
+        ]
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].css',
+              outputPath: '../css',
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              config: {
+                path: path.resolve(__dirname, 'postcss.config.js'),
+                ctx: {
+                  minify: ('production' === environment),
+                }
+              }
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: ('development' === environment),
+              sassOptions: {
+                importer: globImporter()
+              }
+            }
+          },
+        ]
+      },
+      {
+        test: /\.vue$/,
+        use: [{
+          loader: 'vue-loader',
+          options: {
+            loaders: {
+              js: 'babel-loader',
+              options: babelConfig,
+            }
+          }
+        }]
+      },
+      {
+        test: /\.js$/,
+        use: [{
+          loader: 'babel-loader',
+          options: babelConfig,
+        }],
+        exclude: [
+          /node_modules(?!\/\@flickerbox)/,
+        ],
+      },
+      {
+        test: /\.svg$/,
+        loader: 'vue-svg-loader',
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: './fonts'
+          }
+        }]
+      },
+    ]
+  },
 
-		module: {
-			rules: [{
-				test: /\.css$/,
-				use: [{
-					loader: 'vue-style-loader',
-				}, {
-					loader: 'css-loader',
-				}]
-			}, {
-				test: /\.s[ac]ss$/,
-				loader: 'import-glob-loader',
-				enforce: 'pre',
-			}, {
-				test: /\.s[ac]ss$/,
-				use: [{
-					loader: 'file-loader',
-					options: {
-						name: '[name].css',
-						outputPath: './css',
-						sourceMap: true,
-					},
-				}, {
-					loader: 'postcss-loader',
-					options: {
-						config: {
-							path: path.resolve(__dirname, 'postcss.config.js'),
-							ctx: {
-								minify: (env === 'production'),
-							},
-						},
-					},
-				}, {
-					loader: 'sass-loader',
-				}]
-			}, {
-				test: /\.vue$/,
-				use: [{
-					loader: 'vue-loader',
-					options: {
-						loaders: {
-							js: 'babel-loader',
-							options: babelConfig,
-						}
-					}
-				}]
-			}, {
-				test: /\.js$/,
-				use: [{
-					loader: 'babel-loader',
-					options: babelConfig,
-				}],
-				exclude: [
-					/node_modules(?!\/\@flickerbox)/,
-				],
-			}, {
-				test: /\.svg$/,
-				loader: 'vue-svg-loader',
-			}, {
-				test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-				use: [{
-					loader: 'file-loader',
-					options: {
-						name: '[name].[ext]',
-						outputPath: './fonts'
-					}
-				}]
-			}],
-		}
-	}
-
-}
+};
