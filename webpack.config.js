3f733d8c6dc8d7794ba8c0bba189a59f35d8f440
Cody Baker <cody@flickerbox.com>
2020-05-13 13:02:11 -0700

    Revert vue 3 testing

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 const path = require('path');
-const webpack = require('webpack');
-const loader = require('vue-loader');
-
 const babelConfig = require('./babel.config');
 
 const Dashboard = require('webpack-dashboard/plugin');
 const IgnoreAssetsPlugin = require('ignore-assets-webpack-plugin');
 const Notifier = require('webpack-notifier');
+const VueLoader = require('vue-loader/lib/plugin');
+const Webpack = require('webpack');
 
 const environment = process.env.NODE_ENV;
 
@@ -23,7 +22,7 @@ module.exports = {
 
   plugins: [
     new Dashboard(),
-    new loader.VueLoaderPlugin(),
+    new VueLoader(),
     new IgnoreAssetsPlugin({
       ignore: ['js/css.js', 'js/css.js.map']
     }),
@@ -34,16 +33,16 @@ module.exports = {
       skipFirstNotification: false,
       excludeWarnings: false,
     }),
-    new webpack.DefinePlugin({
-      PRODUCTION: JSON.stringify( 'production' === environment ),
+    new Webpack.DefinePlugin({
+      PRODUCTION: JSON.stringify('production' === environment),
     }),
   ],
 
-  devtool: ( 'production' === environment && '' || 'source-map' ),
+  devtool: ( 'production' === environment ? '' : 'source-map' ),
 
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue-bundler.esm.js',
+      'vue$': 'vue/dist/vue.esm.js',
     },
     extensions: ['*', '.js', '.json', '.jsx', '.vue'],
   },
@@ -78,7 +77,7 @@ module.exports = {
               config: {
                 path: path.resolve(__dirname, 'postcss.config.js'),
                 ctx: {
-                  minify: ( 'production' === environment ),
+                  minify: ('production' === environment),
                 }
               }
             }
@@ -87,7 +86,7 @@ module.exports = {
             loader: 'sass-loader',
             options: {
               implementation: require('sass'),
-              sourceMap: ( 'development' === environment ),
+              sourceMap: ('development' === environment),
               sassOptions: {
                 precision: 10,
               }
